a3fa63873328978bbc8bf598fd76366737f9aaf2
Kenneth Illman <kenneth.illman@gmail.com>
2020-11-16 10:51:15 +0100

    meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,7 @@ export default {
     title: 'Purpsot | POC | Nuxt.js',
     meta: [
       { charset: 'utf-8' },
+      { 'http-equiv':'X-UA-Compatible', content:'IE=edge' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' }
     ],
